78eefa50a7cae86889081eb051fa4269df383d2a
Illia <shabelnyk.ilya@gmail.com>
2019-11-10 20:03:27 +0200

    Init and project prepared for future changes

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Header from '../Header'
+import ItemList from '../ItemList'
+import PersonDetails from '../PersonDetails'
+import RandomPlanet from '../RandomPlanet'
+
+import './App.css'
+
+
+const App = () => {
+    return (
+        <div className='container-fluid'>
+            <Header />
+            <RandomPlanet />
+            <div className="row mb-2">
+                <div className="col-md-6">
+                    <ItemList />
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App
